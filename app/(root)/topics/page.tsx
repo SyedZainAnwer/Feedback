32caf8ab75bdc995a80a22b10bde62e3c1dc387d
Syed Zain Anwer <zainanwer24@gmail.com>
2024-02-24 22:21:34 +0100

    route for topics (only for mobile devices)

---
+++ app/(root)/topics/page.tsx
@@ -0,0 +1,20 @@
+import HeadingIndicator from "@/components/shared/HeadingIndicator";
+import { fetchTopics } from "@/lib/actions/post.actions";
+
+const Page = async () => {
+
+    const topics = await fetchTopics()
+
+    return (
+        <div>
+            {topics?.map((topic, i) => (
+                <div key={i} className="flex mt-2">
+                    <HeadingIndicator className="h-5" />
+                    <p>{topic}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Page;
--- components/shared/CommentCard.tsx
+++ components/shared/CommentCard.tsx
@@ -1,6 +1,6 @@
 const CommentCard = ({text}: {text: string}) => {
     return (
-        <div className="border border-light_gray p-4 rounded-tr-2xl rounded-b-2xl mb-3 w-full">
+        <div className="border border-light_gray p-4 rounded-tr-2xl rounded-b-2xl mb-3">
             <p className="text-black">{text}</p>
         </div>
     )
