7555a1917342dcc7c7e6cd2acd7bcf96eeceeff6
Syed Zain Anwer <zainanwer24@gmail.com>
2024-03-01 16:27:22 +0100

    share post link (need to fix)

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useRouter } from "next/navigation";
+import { useRouter as Router } from "next/router";
 import HeadingIndicator from "./shared/HeadingIndicator";
 import PostInteraction from "./shared/PostInteraction";
 import { calculateTime } from "@/lib/utils";
@@ -18,13 +19,18 @@ const Card = ({ topic, text, createdAt, id, isCommentPage, numberOfComments }: P
 
     const timeAgo = createdAt ? calculateTime(new Date(createdAt)) : '';
     const router = useRouter();
+    const share = Router()
 
     const onCommentIconClick = () => {
         router.push(`post/${id}`)
     }
 
     const onShareIconClick = () => {
+        const postLink = process.env.BASE_URL + share.asPath;
 
+        navigator.clipboard.writeText(postLink);
+
+        console.log("post copied")
     }
 
     return (
